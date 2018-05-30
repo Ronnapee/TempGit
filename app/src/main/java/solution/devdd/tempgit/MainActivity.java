366cc63b02607ae3241870b2f7a5fa67fd4b99c8
Ronnapee <j.lnw.ronnapee@gmail.com>
2018-05-30 09:06:57 +0700

    Initial commit

---
+++ app/src/main/java/solution/devdd/tempgit/MainActivity.java
@@ -0,0 +1,13 @@
+package solution.devdd.tempgit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
